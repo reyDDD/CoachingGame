8a1947a31d339c969323e5026d0ce734b036b8fd
o.andrusik <o.andrusik@e-docs.ua>
2022-09-16 14:46:57 +0300

    fix bag in logic

---
+++ TamboliyaLibrary/Models/ActionType.cs
@@ -0,0 +1,12 @@
+﻿
+namespace TamboliyaLibrary.Models
+{
+	public enum ActionType
+	{
+		NotSet = 0,
+		NewStep = 1,
+		RandomAction = 2,
+		UserVote = 3,
+		GoToSelectPosition = 4
+	}
+}
---
+++ TamboliyaLibrary/Models/MoveModel.cs
@@ -0,0 +1,12 @@
+﻿
+namespace TamboliyaLibrary.Models
+{
+	public class MoveModel
+	{
+		public int GameId { get; set; }
+		public Guid? UserId { get; set; }
+		public ActionType ActionType { get; set; }
+		public RegionOnMap RegionOnMap { get; set; }
+		public int? PositionNumber { get; set; }
+	}
+}
