ff80191d15fc3f88728fd5bed98b1773a4690ae5
o.andrusik <o.andrusik@e-docs.ua>
2023-10-22 16:28:53 +0200

    Revert "add position after click on videoTrack"
    This reverts commit 8921aa30d04075e0321fa0070910a0378bbcc9dc.

--- Shared/Rtc.razor.js
+++ Shared/Rtc.razor.js
@@ -11,7 +11,7 @@ export function setRemoteStream(stream) {
         document.getElementById(`remoteVideo-${stream[0].id}`);
         let video = document.getElementById(`remoteVideo-${stream[0].id}`);
         if (video === null) {
-            let videoBlock = `<div class="video__container video-player" id="user-container-${stream[0].id}" style="width: 200px; height: 200px; border-radius: 50%; object-fit: cover; display: flex; justify-content: center; align-items: center; border: 2px solid #b366f9; cursor: pointer; overflow: hidden;"><video class="video-player" id="remoteVideo-${stream[0].id}" autoplay playsinline></video></div>`;
+            let videoBlock = `<div class="video__container video-player" id="user-container-${stream[0].id}" style="width: 200px; height: 200px; border-radius: 50%; object-fit: cover; display: flex; justify-content: center; align-items: center; border: 2px solid #b366f9; border-radius: 50%; cursor: pointer; overflow: hidden;"><video class="video-player" id="remoteVideo-${stream[0].id}" autoplay playsinline></video></div>`;
             remoteVideoContainer.insertAdjacentHTML("beforeend", videoBlock);
             video = document.getElementById(`remoteVideo-${stream[0].id}`);
             video.srcObject = stream[0];
@@ -57,8 +57,6 @@ let userIdInDisplayFrame = null;
 let expandVideoFrame = (e) => {
     let child = displayFrame.children[0];
     if (child) {
-        e.currentTarget.style = "height: 100% !important; width: 100 % !important; object-fit: cover; display: flex; justify-content: center; align-items: center; border: 2px solid #b366f9; border-radius: 0; cursor: pointer; overflow: hidden;";
-        child.style = "width: 200px; height: 200px; object-fit: cover; display: flex; justify-content: center; align-items: center; border: 2px solid #b366f9; border-radius: 50%; cursor: pointer; overflow: hidden;";
         document.getElementById('streams__container').appendChild(child);
     }
 
@@ -67,8 +65,6 @@ let expandVideoFrame = (e) => {
     userIdInDisplayFrame = e.currentTarget.id;
 }
 
-export function addEventListenerForVideoFrames() {
-    for (let i = 0; videoFrames.length > i; i++) {
-        videoFrames[i].addEventListener('click', expandVideoFrame)
-    }
+for (let i = 0; videoFrames.length > i; i++) {
+    videoFrames[i].addEventListener('click', expandVideoFrame)
 }
